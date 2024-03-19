f2253b90a7bc1f264d0186de349323561f3e4902
Josh-Exton <145766031+Josh-Exton@users.noreply.github.com>
2024-03-19 22:29:34 +0000

    Update Program.cs

--- Algorithms and Complexity Assignment 1/Program.cs
+++ Algorithms and Complexity Assignment 1/Program.cs
@@ -15,133 +15,16 @@ namespace Algorithms_and_Complexity_Assignment_1
         static void Main(string[] args)
         {
             Sorting sort = new Sorting();
-            string currentDirectory = Directory.GetCurrentDirectory();
-            string fileName = "Net_1_256.txt";
-            string filePath = Path.Combine(currentDirectory, fileName);
-            string lineContent;
-            int arrayIndex = 0;
-            try
-            {
-                using (StreamReader Streamreader = new StreamReader(filePath))
-                {
-                    int lineCounter = File.ReadAllLines(filePath).Length;
-                    int [] Net_1_256Array = new int[lineCounter];
-                    lineContent = Streamreader.ReadLine();
-                    while (lineContent != null)
-                    {
-                        int.TryParse(lineContent, out int number);
-                        Net_1_256Array[arrayIndex] = number;
-                        arrayIndex++;
-                        lineContent = Streamreader.ReadLine();
-                    }
-                }
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine("An error occurred while opening the file:");
-                Console.WriteLine(e.Message);
-            }
-
-            fileName = "Net_2_256.txt";
-            filePath = Path.Combine(currentDirectory, fileName);
-            arrayIndex = 0;
-            try
-            {
-                using (StreamReader Streamreader = new StreamReader(filePath))
-                {
-                    int lineCounter = File.ReadAllLines(filePath).Length;
-                    int[] Net_2_256Array = new int[lineCounter];
-                    lineContent = Streamreader.ReadLine();
-                    while (lineContent != null)
-                    {
-                        int.TryParse(lineContent, out int number);
-                        Net_2_256Array[arrayIndex] = number;
-                        arrayIndex++;
-                        lineContent = Streamreader.ReadLine();
-                    }
-                }
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine("An error occurred while opening the file:");
-                Console.WriteLine(e.Message);
-            }
-
-            fileName = "Net_3_256.txt";
-            filePath = Path.Combine(currentDirectory, fileName);
-            arrayIndex = 0;
-            try
-            {
-                using (StreamReader Streamreader = new StreamReader(filePath))
-                {
-                    int lineCounter = File.ReadAllLines(filePath).Length;
-                    int[] Net_3_256Array = new int[lineCounter];
-                    lineContent = Streamreader.ReadLine();
-                    while (lineContent != null)
-                    {
-                        int.TryParse(lineContent, out int number);
-                        Net_3_256Array[arrayIndex] = number;
-                        arrayIndex++;
-                        lineContent = Streamreader.ReadLine();
-                    }
-                }
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine("An error occurred while opening the file:");
-                Console.WriteLine(e.Message);
-            }
-
-            fileName = "Net_1_2048.txt";
-            filePath = Path.Combine(currentDirectory, fileName);
-            arrayIndex = 0;
-            try
-            {
-                using (StreamReader Streamreader = new StreamReader(filePath))
-                {
-                    int lineCounter = File.ReadAllLines(filePath).Length;
-                    int[] Net_1_2048Array = new int[lineCounter];
-                    lineContent = Streamreader.ReadLine();
-                    while (lineContent != null)
-                    {
-                        int.TryParse(lineContent, out int number);
-                        Net_1_2048Array[arrayIndex] = number;
-                        arrayIndex++;
-                        lineContent = Streamreader.ReadLine();
-                    }
-                }
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine("An error occurred while opening the file:");
-                Console.WriteLine(e.Message);
-            }
-
-            fileName = "Net_2_2048.txt";
-            filePath = Path.Combine(currentDirectory, fileName);
-            arrayIndex = 0;
-            try
-            {
-                using (StreamReader Streamreader = new StreamReader(filePath))
-                {
-                    int lineCounter = File.ReadAllLines(filePath).Length;
-                    int[] Net_2_2048Array = new int[lineCounter];
-                    lineContent = Streamreader.ReadLine();
-                    while (lineContent != null)
-                    {
-                        int.TryParse(lineContent, out int number);
-                        Net_2_2048Array[arrayIndex] = number;
-                        arrayIndex++;
-                        lineContent = Streamreader.ReadLine();
-                    }
-                }
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine("An error occurred while opening the file:");
-                Console.WriteLine(e.Message);
-            }
-
+            Searching search = new Searching();
+            Files file = new Files();
+            int[] unsortedNet_1_256 = file.TxtFile("Net_1_256.txt");
+            int[] unsortedNet_2_256 = file.TxtFile("Net_2_256.txt");
+            int[] unsortedNet_3_256 = file.TxtFile("Net_3_256.txt");
+            int[] unsortedNet_1_2048 = file.TxtFile("Net_1_2048.txt");
+            int[] unsortedNet_2_2048 = file.TxtFile("Net_2_2048.txt");
+            int[] unsortedNet_3_2048 = file.TxtFile("Net_3_2048.txt");
+            int[] unsortedNet_1_256Net_3_256Merged = unsortedNet_1_256.Concat(unsortedNet_3_256).ToArray();
+            int[] unsortedNet_1_2048Net_3_2048Merged = unsortedNet_1_2048.Concat(unsortedNet_3_2048).ToArray();
             Console.ReadKey();
         }
     }
