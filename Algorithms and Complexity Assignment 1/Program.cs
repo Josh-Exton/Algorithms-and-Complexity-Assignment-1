cacc4891fb9e8231479044a4b52f795621611492
Josh-Exton <145766031+Josh-Exton@users.noreply.github.com>
2024-03-20 21:27:57 +0000

    Update Program.cs

--- Algorithms and Complexity Assignment 1/Program.cs
+++ Algorithms and Complexity Assignment 1/Program.cs
@@ -1,12 +1,4 @@
-using Microsoft.VisualBasic;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.ConstrainedExecution;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Algorithms_and_Complexity_Assignment_1
 {
@@ -14,56 +6,59 @@ namespace Algorithms_and_Complexity_Assignment_1
     {
         static void Main(string[] args)
         {
-            Sorting sort = new Sorting();
-            Searching search = new Searching();
             Files file = new Files();
             Executing execute = new Executing();
 
-            string fileName;
-
             Console.WriteLine("Do you want to open Net_1_256.txt, Net_2_256.txt, Net_3_256.txt");
-            fileName = Console.ReadLine();
-            int[] unsorted256 = file.TxtFile(fileName);
+            string fileName = Console.ReadLine();
+            int[] unsorted256 = file.TxtFileToArray(fileName);
             while (unsorted256.Length == 0) 
             {
                 Console.WriteLine();
                 Console.WriteLine("Do you want to open Net_1_256.txt, Net_2_256.txt, Net_3_256.txt");
                 fileName = Console.ReadLine();
-                unsorted256 = file.TxtFile(fileName);
+                unsorted256 = file.TxtFileToArray(fileName);
             }
 
-            execute.Run(unsorted256);
+            execute.AlgorithmsExecuted(unsorted256);
 
+            Console.WriteLine();
             Console.WriteLine("Do you want to open Net_1_2048.txt, Net_2_2048.txt, Net_3_2048.txt");
             fileName = Console.ReadLine();
-            int[] unsorted2048 = file.TxtFile(fileName);
+            int[] unsorted2048 = file.TxtFileToArray(fileName);
             while (unsorted2048.Length == 0)
             {
                 Console.WriteLine();
                 Console.WriteLine("Do you want to open Net_1_2048.txt, Net_2_2048.txt, Net_3_2048.txt");
                 fileName = Console.ReadLine();
-                unsorted256 = file.TxtFile(fileName);
+                unsorted2048 = file.TxtFileToArray(fileName);
             }
 
-            execute.Run(unsorted2048);
+            execute.AlgorithmsExecuted(unsorted2048);
 
             fileName = "Net_1_256.txt";
-            int[] Net_1_256 = file.TxtFile(fileName);
+            int[] Net_1_256 = file.TxtFileToArray(fileName);
             fileName = "Net_3_256.txt";
-            int[] Net_3_256 = file.TxtFile(fileName);
+            int[] Net_3_256 = file.TxtFileToArray(fileName);
             int[] Unsorted256Merged = Net_1_256.Concat(Net_3_256).ToArray();
 
-            execute.Run(unsorted256);
+            Console.WriteLine();
+            Console.WriteLine("This is the 256 merged array");
+            execute.AlgorithmsExecuted(Unsorted256Merged);
 
             fileName = "Net_1_2048.txt";
-            int[] Net_1_2048 = file.TxtFile(fileName);
+            int[] Net_1_2048 = file.TxtFileToArray(fileName);
             fileName = "Net_3_2048.txt";
-            int[] Net_3_2048 = file.TxtFile(fileName);
+            int[] Net_3_2048 = file.TxtFileToArray(fileName);
             int[] Unsorted2048Merged = Net_1_2048.Concat(Net_3_2048).ToArray();
 
-            execute.Run(unsorted2048);
+            Console.WriteLine();
+            Console.WriteLine("This is the 2048 merged array");
+            execute.AlgorithmsExecuted(Unsorted2048Merged);
 
+            Console.WriteLine();
+            Console.WriteLine("End of program");
             Console.ReadKey();
         }
     }
-}
+} 
